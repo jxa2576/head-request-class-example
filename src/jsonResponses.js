6148f4b41555dba4d5af58f96c36c1dda9d5e9f4
Jeb A <jxa2576@g.rit.edu>
2020-09-10 13:14:47 -0400

    Head Request Example

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -2,31 +2,55 @@
 const users = {};
 
 const respondJSON = (request, response, status, object) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
 
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
 
+  response.writeHead(status, headers);
+  response.end();
 };
 
 const getUsers = (request, response) => {
+  const responseJSON = {
+    users,
+  };
 
+  return respondJSON(request, response, 200, responseJSON);
 };
 
-const getUsersMeta = (request, response) => {
-
-};
+const getUsersMeta = (request, response) => respondJSON(request, response, 200);
 
 const updateUser = (request, response) => {
+  const newUser = {
+    createdAt: Date.now(),
+  };
 
+  users[newUser.createdAt] = newUser;
+
+  return respondJSON(request, response, 201, newUser);
 };
 
 const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found!',
+    id: 'notFound',
+  };
 
+  return respondJSON(request, response, 404, responseJSON);
 };
 
 const notFoundMeta = (request, response) => {
-
+  return respondJSONMeta(request, response, 404);
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -5,10 +5,31 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+const urlStruct = {
+  'GET': {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    '/updateUser': jsonHandler.updateUser,
+    notFound: jsonHandler.notFound,
+  },
+  'HEAD': {
+    '/getUsers': jsonHandler.getUsersMeta,
+    notFound: jsonHandler.notFoundMeta,
+  },
+};
+
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
 
+  console.dir(parsedUrl.pathname);
+  console.dir(request.method);
 
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else {
+    urlStruct[request.method].notFound(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port);
